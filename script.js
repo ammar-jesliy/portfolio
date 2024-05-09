6281b3d218ac60c98d56ec41288079358e9f022d
ammar-jesliy <ammarjc1@gmail.com>
2024-05-09 15:55:48 +0530

    Add responsive nav

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 let headerLogo = document.querySelector(".header-logo");
 
+const nav = document.querySelector('header ul');
+const navToggle = document.querySelector('.nav-toggle');
+
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 100) {
-        headerLogo.classList.remove("header-logo");
-        headerLogo.classList.add("header-logo-shrinked");
-    } else {
-        headerLogo.classList.remove("header-logo-shrinked");
-        headerLogo.classList.add("header-logo");
+    if (window.innerWidth >= 780) {
+        if (window.scrollY > 100) {
+            headerLogo.classList.remove("header-logo");
+            headerLogo.classList.add("header-logo-shrinked");
+        } else {
+            headerLogo.classList.remove("header-logo-shrinked");
+            headerLogo.classList.add("header-logo");
+        }
     }
 })
+
+
+navToggle.addEventListener("click", () => {
+    const visibility = nav.getAttribute('data-visible');
+
+    if (visibility === "false") {
+        nav.setAttribute('data-visible', true);
+    } else {
+        nav.setAttribute('data-visible', false);
+    }
+});
+
+
+function toggleMenu() {
+    let menuButton = document.querySelector(".nav-toggle");
+    if (menuButton.innerHTML === "Menu") {
+      menuButton.innerHTML = "&#10006;"; 
+    } else {
+      menuButton.innerHTML = "Menu";
+    }
+  }
+  
