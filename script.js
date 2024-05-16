b25f1fd57d364ebd8f97a00af08f5ca7fb2bf385
ammar-jesliy <ammarjc1@gmail.com>
2024-05-16 12:44:27 +0530

    Fix height problem

--- script.js
+++ script.js
@@ -5,21 +5,37 @@ const navToggle = document.querySelector('.nav-toggle');
 
 let cursor = document.querySelector(".cursor");
 
+// Indicator to show if the window can fit both .header-logo and .header-content
+let canFit = true
+
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
 
+if (document.querySelector('.hero-content').clientHeight >= (window.innerHeight - document.querySelector('.header-logo').clientHeight - 120)) {
+    headerLogo.classList.add("header-logo-shrinked");
+    canFit = false;
+}
+window.addEventListener('resize', function () {
+    if (document.querySelector('.hero-content').clientHeight >= (window.innerHeight - document.querySelector('.header-logo').clientHeight - 120)) {
+        headerLogo.classList.add("header-logo-shrinked");
+        canFit = false;
+    } else {
+        headerLogo.classList.remove("header-logo-shrinked");
+        canFit = true;
+    }
+})
+
 window.addEventListener('scroll', function() {
-    if (window.innerWidth >= 780) {
+    if (window.innerWidth >= 780 && canFit) {
         if (window.scrollY > 10) {
-            headerLogo.classList.remove("header-logo");
             headerLogo.classList.add("header-logo-shrinked");
         } else {
             headerLogo.classList.remove("header-logo-shrinked");
-            headerLogo.classList.add("header-logo");
         }
     }
 })
+
 
 
 navToggle.addEventListener("click", () => {
