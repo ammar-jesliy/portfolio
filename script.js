86f22d5e197f977e435e9eab6b352d485fb238a8
ammar-jesliy <ammarjc1@gmail.com>
2024-06-06 11:14:34 +0530

    Add sliding projects section, fix carousel, adjust about height

--- script.js
+++ script.js
@@ -1,128 +1,136 @@
 let headerLogo = document.querySelector(".header-logo");
 
-const nav = document.querySelector('header ul');
-const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector("header ul");
+const navToggle = document.querySelector(".nav-toggle");
 
 let cursor = document.querySelector(".cursor");
 
 // Indicator to show if the window can fit both .header-logo and .header-content
-let canFit = true
+let canFit = true;
 
 window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-}
+  window.scrollTo(0, 0);
+};
 
-if (document.querySelector('.hero-content').clientHeight >= (window.innerHeight - document.querySelector('.header-logo').clientHeight - 120)) {
+if (
+  document.querySelector(".hero-content").clientHeight >=
+  window.innerHeight - document.querySelector(".header-logo").clientHeight - 120
+) {
+  headerLogo.classList.add("header-logo-shrinked");
+  canFit = false;
+}
+window.addEventListener("resize", function () {
+  if (
+    document.querySelector(".hero-content").clientHeight >=
+    window.innerHeight -
+      document.querySelector(".header-logo").clientHeight -
+      120
+  ) {
     headerLogo.classList.add("header-logo-shrinked");
     canFit = false;
-}
-window.addEventListener('resize', function () {
-    if (document.querySelector('.hero-content').clientHeight >= (window.innerHeight - document.querySelector('.header-logo').clientHeight - 120)) {
-        headerLogo.classList.add("header-logo-shrinked");
-        canFit = false;
-    } else {
-        headerLogo.classList.remove("header-logo-shrinked");
-        canFit = true;
-    }
-})
-
-window.addEventListener('scroll', function() {
-    if (window.innerWidth >= 780 && canFit) {
-        if (window.scrollY > 10) {
-            headerLogo.classList.add("header-logo-shrinked");
-        } else {
-            headerLogo.classList.remove("header-logo-shrinked");
-        }
-    }
-})
-
-
-
-navToggle.addEventListener("click", () => {
-    const visibility = nav.getAttribute('data-visible');
-
-    if (visibility === "false") {
-        nav.setAttribute('data-visible', true);
-    } else {
-        nav.setAttribute('data-visible', false);
-    }
+  } else {
+    headerLogo.classList.remove("header-logo-shrinked");
+    canFit = true;
+  }
 });
 
+window.addEventListener("scroll", function () {
+  if (window.innerWidth >= 780 && canFit) {
+    if (window.scrollY > 10) {
+      headerLogo.classList.add("header-logo-shrinked");
+    } else {
+      headerLogo.classList.remove("header-logo-shrinked");
+    }
+  }
+});
+
+navToggle.addEventListener("click", () => {
+  const visibility = nav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    nav.setAttribute("data-visible", true);
+  } else {
+    nav.setAttribute("data-visible", false);
+  }
+});
 
 function toggleMenu() {
-    let menuButton = document.querySelector(".nav-toggle");
-    if (menuButton.innerHTML === "Menu") {
-      menuButton.innerHTML = "&#10006;"; 
-    } else {
-      menuButton.innerHTML = "Menu";
-    }
+  let menuButton = document.querySelector(".nav-toggle");
+  if (menuButton.innerHTML === "Menu") {
+    menuButton.innerHTML = "&#10006;";
+  } else {
+    menuButton.innerHTML = "Menu";
+  }
 }
-
 
 // Custom cursor smooth animation
 let mouseX;
 let mouseY;
 
 function isTouchDevice() {
-    return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+  return (
+    "ontouchstart" in window ||
+    navigator.maxTouchPoints > 0 ||
+    navigator.msMaxTouchPoints > 0
+  );
 }
 
 if (!isTouchDevice()) {
-    document.addEventListener('mousemove', function(e) {
-        mouseX = e.clientX;
-        mouseY = e.clientY;
+  document.addEventListener("mousemove", function (e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+
+  document.querySelectorAll("p, h1, h2, h3, .hero-text").forEach((p) => {
+    p.addEventListener("mouseenter", () => {
+      cursor.classList.add("hover");
     });
-
-    document.querySelectorAll('p, h1, h2, h3, .hero-text').forEach(p => {
-        p.addEventListener('mouseenter', () => {
-            cursor.classList.add('hover');
-        });
-        p.addEventListener('mouseleave', () => {
-            cursor.classList.remove('hover');
-        });
+    p.addEventListener("mouseleave", () => {
+      cursor.classList.remove("hover");
     });
+  });
 
-    document.querySelectorAll('input, textarea').forEach(p => {
-        p.addEventListener('mouseenter', () => {
-            cursor.classList.add('hover-input');
-            if (p.type == "submit") {
-                cursor.innerHTML = "go.";
-            } else {
-                cursor.innerHTML = "enter."
-            }
-        });
-        p.addEventListener('mouseleave', () => {
-            cursor.classList.remove('hover-input');
-            cursor.innerHTML = "";
-        });
+  document.querySelectorAll("input, textarea").forEach((p) => {
+    p.addEventListener("mouseenter", () => {
+      cursor.classList.add("hover-input");
+      if (p.type == "submit") {
+        cursor.innerHTML = "go.";
+      } else {
+        cursor.innerHTML = "enter.";
+      }
     });
-
-    document.querySelectorAll('a').forEach(p => {
-        p.addEventListener('mouseenter', () => {
-            cursor.classList.add('hover-link');
-            cursor.innerHTML = "visit.";
-        });
-        p.addEventListener('mouseleave', () => {
-            cursor.classList.remove('hover-link');
-            cursor.innerHTML = "";
-        });
+    p.addEventListener("mouseleave", () => {
+      cursor.classList.remove("hover-input");
+      cursor.innerHTML = "";
     });
+  });
 
-    const nameSvg = document.querySelector('.header-logo svg');
-
-    nameSvg.addEventListener('mouseenter', () => {
-        cursor.classList.add('hover-invert');
+  document.querySelectorAll("a").forEach((p) => {
+    p.addEventListener("mouseenter", () => {
+      cursor.classList.add("hover-link");
+      cursor.innerHTML = "visit.";
     });
-    nameSvg.addEventListener('mouseleave', () => {
-        cursor.classList.remove('hover-invert');
+    p.addEventListener("mouseleave", () => {
+      cursor.classList.remove("hover-link");
+      cursor.innerHTML = "";
     });
+  });
 
-    function animate() {
-        cursor.style.cssText = "left: " + mouseX + "px; top: " + mouseY + "px;";
-        requestAnimationFrame(animate);
-    }
+  const nameSvg = document.querySelector(".header-logo svg");
 
+  nameSvg.addEventListener("mouseenter", () => {
+    cursor.classList.add("hover-invert");
+  });
+  nameSvg.addEventListener("mouseleave", () => {
+    cursor.classList.remove("hover-invert");
+  });
+
+  function animate() {
+    cursor.style.cssText = "left: " + mouseX + "px; top: " + mouseY + "px;";
     requestAnimationFrame(animate);
+  }
+
+  requestAnimationFrame(animate);
 } else {
-    cursor.style.display = "none";
+  cursor.style.display = "none";
 }
