1e73151b8622a628951b335bd153cbbf3d5c7883
ammar-jesliy <ammarjc1@gmail.com>
2024-06-19 10:50:28 +0530

    Refactor code, adjust magnet buttons, add reload on resize

--- script.js
+++ script.js
@@ -120,3 +120,37 @@ const timeZone = "Asia/Colombo";
 const now = new Date().toLocaleTimeString("en-US", {timeZone, hour: "2-digit", minute: "numeric", hour12: true});
 
 document.querySelector(".time p").innerHTML = `${now} GMT+5:30`;
+
+
+// magnetic button effect
+// from via: https://codepen.io/tdesero/pen/RmoxQg
+var magnets = document.querySelectorAll(".magnetic");
+var strength = 30;
+
+magnets.forEach((magnet) => {
+  magnet.addEventListener("mousemove", moveMagnet);
+  magnet.addEventListener("mouseout", function (event) {
+    TweenMax.to(event.currentTarget, 1, {
+      x: 0,
+      y: 0,
+      ease: Elastic.easeOut,
+    });
+  });
+});
+
+function moveMagnet(event) {
+  var magnetButton = event.currentTarget;
+  var bounding = magnetButton.getBoundingClientRect();
+
+  TweenMax.to(magnetButton, 1, {
+    x:
+      ((event.clientX - bounding.left) / magnetButton.offsetWidth -
+        0.5) *
+      strength,
+    y:
+      ((event.clientY - bounding.top) / magnetButton.offsetHeight -
+        0.5) *
+      strength,
+    ease: Power4.easeOut,
+  });
+}
